a63ccf0a962ceffbe1702e4a8ccded0003376d11
Joshi-Atharva <atharva.a.j234@gmail.com>
2024-08-25 10:51:11 +0530

    Update model.py

--- model.py
+++ model.py
@@ -7,10 +7,10 @@ class Sobel(nn.Module):
         super().__init__()
         self.filter = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, stride=1, padding=0, bias=False)
 
-        Gx = torch.tensor([[2.0, 0.0, -2.0], [4.0, 0.0, -4.0], [2.0, 0.0, -2.0]])
-        Gy = torch.tensor([[2.0, 4.0, 2.0], [0.0, 0.0, 0.0], [-2.0, -4.0, -2.0]])
-        G = torch.cat([Gx.unsqueeze(0), Gy.unsqueeze(0)], 0)
-        G = G.unsqueeze(1)
+        Gx = torch.tensor([[2.0, 0.0, -2.0], [4.0, 0.0, -4.0], [2.0, 0.0, -2.0]]) # sobel operator for horizontal edge detection
+        Gy = torch.tensor([[2.0, 4.0, 2.0], [0.0, 0.0, 0.0], [-2.0, -4.0, -2.0]]) # for vertical edge detection
+        G = torch.cat([Gx.unsqueeze(0), Gy.unsqueeze(0)], 0) # 0th dimension (axis 0) represents number of filters = out_channels
+        G = G.unsqueeze(1) # dimension 1 (axis 1) represents in_channels 
         self.filter.weight = nn.Parameter(G, requires_grad=False)
 
     def forward(self, img):
